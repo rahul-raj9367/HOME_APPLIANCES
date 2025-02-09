4213986a95eac1bf4a802005c608c2af1d6fe721
rahulraj.p <rahulraj.p@zohocorp.com>
2025-02-09 16:28:09 +0530

    add star pattern program

--- main.java
+++ main.java
@@ -6,6 +6,15 @@ public class HelloWorld {
         for (int i = 0; i <= 5; i++) {
             System.out.println(i);
         }
+
+        // Print a simple star pattern
+        System.out.println("Star Pattern:");
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
 
