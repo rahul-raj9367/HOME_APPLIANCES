ae4ac4cfd5f8a7e414d7165f032410167bbfa496
rahulraj.p <rahulraj.p@zohocorp.com>
2025-02-09 15:15:31 +0530

    Write the program print 0 to 5

--- main.java
+++ main.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        // Print numbers from 0 to 5
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(i);
+        }
     }
 }
+
